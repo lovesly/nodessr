77a12d17f957bbd046ce7e3792b259562462d376
zzh <zzhlovesly@outlook.com>
2020-03-04 00:44:32 +0800

    refactor commonjs to es6 module

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
-
+/**
+ * nodemon 加上 webpack --watch 实现了，代码改变 rerun webpack，并且 rerun server
+ * 那么除了上面的底层实现，浏览器自动重新加载，不用手动刷新，要怎么做呢？？
+ */
 const Home = () => {
-    const [name, setName] = useState('ZZ');
+    const [name, setName] = useState('ZZZ');
     // well, apparently it's not working in this way
     return (
         [
             <div>I'm the home component, { name }</div>,
-            <h1 onClick={() => setName('SLY')}>Click me</h1>
+            <button onClick={() => setName('SLY')}>Click me</button>
         ]
     );
 };
