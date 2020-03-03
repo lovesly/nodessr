2048e21263fb5257e0bc1aeef15cc90c098892d0
zzh <zzhlovesly@outlook.com>
2020-03-03 23:50:35 +0800

    click event not working?

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 
 const Home = () => {
     const [name, setName] = useState('ZZ');
+    // well, apparently it's not working in this way
     return (
-        <>
-            <div>I'm the home component, { name }</div>
-            <h1 onClick={setName('SLY')}>Click me</h1>
-        </>
+        [
+            <div>I'm the home component, { name }</div>,
+            <h1 onClick={() => setName('SLY')}>Click me</h1>
+        ]
     );
 };
 
