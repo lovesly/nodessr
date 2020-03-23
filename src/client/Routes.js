74a5d8c10b874ea9f37336c63a20d34e2d4ffe22
zzh <zzhlovesly@outlook.com>
2020-03-24 00:50:18 +0800

    modify routes to show header

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import { Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import UsersListPage from './pages/UsersListPage';
+import App from './app';
 
 // export default () => {
 //     return (
@@ -14,11 +15,17 @@ import UsersListPage from './pages/UsersListPage';
 
 // to use react-router-config??
 // 个人感觉完全可以通过 babel 插件做吧？没有什么特别了不起的，虽然肯定比我了不起。。。
+// 嵌套是怎么做的？
 export default [{
-    ...HomePage,
-    path: '/',
-    exact: true
-}, {
-    ...UsersListPage,
-    path: '/users',
+    ...App,
+    routes: [
+        {
+            ...HomePage,
+            path: '/',
+            exact: true
+        }, {
+            ...UsersListPage,
+            path: '/users',
+        }
+    ]
 }];
